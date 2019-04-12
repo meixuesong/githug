691ee8d856a00eee5c5462e4752a516a5e1230b7
Mei Xuesong <meixuesong@gmail.com>
2019-04-12 17:32:20 +0800

    finish alias lg

--- levels/alias_lg.rb
+++ levels/alias_lg.rb
@@ -3,6 +3,39 @@ description "使⽤git lg来查看带有树形结构的提交记录."
 
 setup do
   repo.init
+
+  FileUtils.touch "file1"
+  repo.add        "file1"
+  repo.commit_all "committed changes on master"
+
+  # add another file.
+  FileUtils.touch "file2"
+  repo.add        "file2"
+  repo.commit_all "add another file: file2"
+
+  `git checkout -b other_branch --quiet`
+  # add another file
+  FileUtils.touch "file3"
+  repo.add        "file3"
+  # `echo "hello" >> file2`
+  # repo.add        "file2"  
+  repo.commit_all "add another file: file3"
+
+  # `git checkout -b test_branch --quiet`
+
+  # FileUtils.touch "file4"
+  # repo.add        "file4"
+  # repo.commit_all "committed change on test_branch"
+
+  `git checkout master`
+
+  FileUtils.touch "file5"
+  repo.add        "file5"
+  repo.commit_all "add another file: file5"
+
+  `git merge other_branch -m "merge other_branch"`
+  # `git merge test_branch`
+  
 end
 
 solution do
