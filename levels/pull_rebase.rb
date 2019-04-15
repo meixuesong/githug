64830b38ee6a7a65d55a663157ef50e4bcbfa8cd
Mei Xuesong <meixuesong@gmail.com>
2019-04-15 16:54:52 +0800

    finish pull rebase

--- levels/pull_rebase.rb
+++ levels/pull_rebase.rb
@@ -4,60 +4,44 @@ description "你需获取远程所有分⽀最新代码，并让版本树尽量�
 setup do
   # remember the working directory so we can come back to it later
   cwd = Dir.pwd
-  # initialize another git repo to be used as a "remote"
-  tmpdir = Dir.mktmpdir
-
-  # local repo
   repo.init
 
-  FileUtils.touch "file1"
-  `echo "I am saying Hello." >> file1`
-  repo.add        "file1"
-  repo.commit_all "First commit"
-
-  # copy the repo to remote
-  # FileUtils.cp "file1", tmpdir
-  # FileUtils.cp "file2", tmpdir
-
-  # repo.add        "file1"
-  # repo.commit_all "Second commit"
-
-  # add another file
-  # FileUtils.touch "file3"
-  # repo.add        "file3"
-  # repo.commit_all "Third commit"
-
+  # initialize another git repo to be used as a "remote"
   # remote repo
+  tmpdir = Dir.mktmpdir
   Dir.chdir tmpdir
   repo.init
   # make a 'non-bare' repo accept pushes
   `git config receive.denyCurrentBranch ignore`
 
   # add a different file and commit so remote and local would diverge
-  FileUtils.touch "file2"
-  repo.add        "file2"
-  repo.commit_all "A commit by teammate"
+  FileUtils.touch "file1"
+  repo.add        "file1"
+  repo.commit_all "first commit"
 
   # change back to original repo to set up a remote
   Dir.chdir cwd
-  `git remote add origin #{tmpdir}/.git`
-  `git fetch origin`
-  `git branch -u origin/master master 2> /dev/null`
+  `git remote add origin #{tmpdir}/.git  2> /dev/null`
+  `git fetch origin  2> /dev/null`
+  `git branch -u origin/master master  2> /dev/null`
+
+  # 处理本地repo
+  `git pull origin master --quiet`
+  `echo "Hello" >> file1 && git add file1  2> /dev/null && git commit -m "Second commit"  2> /dev/null`
+
+  Dir.chdir tmpdir
+  FileUtils.touch "file2"
+  `echo "Hello" >> file2 && git add file2  2> /dev/null && git commit -m "Third commit"  2> /dev/null`
+
+  Dir.chdir cwd
+  FileUtils.touch "file3"
+  `echo "Hello" >> file3  && git add file3  2> /dev/null && git commit -m "Forth commit"  2> /dev/null`
+  `git branch --set-upstream-to=origin/master master  2> /dev/null`
+
 end
 
 solution do
-  # repo.init
-  # result = true
-
-  # # Check the commits of the local branch and the branch are the same.
-  # local_commits = repo.commits("master")
-  # remote_commits = repo.commits("origin/master")
-  # result = false unless local_commits.size == 4
-  # local_commits.each_with_index do |commit, idx|
-  #   result &&= (commit.id == remote_commits[idx].id)
-  # end
-
-  # result
+  1 == repo.commits("master")[0].parents.length
 end
 
 hint do
